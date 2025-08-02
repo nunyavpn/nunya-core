e743e3d007844d5dad9737c4bbfc91e31dd13560
Mahdi <80265960+Mahdi-zarei@users.noreply.github.com>
2025-08-02 13:49:32 +0330

    Migrate to protorpc (#598)
    * refactor: migrate from grpc to protorpc
    
    * fix
    
    * fix
    
    * fix
    
    * cleanup
    
    * Update mainwindow_grpc.cpp
    
    * Update RPC.cpp
    
    * fix
    
    ---------
    
    Co-authored-by: parhelia512 <0011d3@gmail.com>

--- server/server_linux.go
+++ server/server_linux.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"Core/gen"
-	"Core/internal/boxdns"
 )
 
 func (s *server) SetSystemDNS(in *gen.SetSystemDNSRequest, out *gen.EmptyResp) error {
-	err := boxdns.DnsManagerInstance.SetSystemDNS(nil, *in.Clear)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
